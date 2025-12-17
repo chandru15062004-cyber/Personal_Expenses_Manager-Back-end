fbdd315bfd7d6a43b34c116ff4047c4aa5a003b9
Chandru C <chandru15062004@gmail.com>
2025-12-17 17:59:05 +0530

    Add files via upload

---
+++ PersonalExpenseManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalExpenseManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalExpenseManagerApplication.class, args);
+	}
+
+}
